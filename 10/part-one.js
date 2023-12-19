fce587f2b5624958606b16b0288eab009446ae30
Josh Bishop <jbishop45@gatech.edu>
2023-12-19 13:22:33 -0600

    day 10 part 1

--- 10/part-one.js
+++ 10/part-one.js
@@ -1,48 +1,98 @@
 const {input} = require('./input');
+// const input = [
+//       '....|',
+//       '..F-J',
+//       '.FS7.',
+//       '.L-J.',
+//       '.....'
+// ]
 
-mask = Array(input.length).fill(Array(input[0].length).join('1'));
-trajectory = [];
+const connections = {
+      'N': ['|','L','J','S'],
+      'E': ['-','L','F','S'],
+      'S': ['|','7','F','S'],
+      'W': ['-','J','7','S']
+};
 
 // Find Starting Point
+let s_i = -1, s_j = -1;
 for (let i=0; i<input.length; i++) {
       for (let j=0; j<input[0].length; j++) {
             if (input[i][j] == 'S') {
-                  const s_i = i;
-                  const s_j = j;
+                  s_i = i; 
+                  s_j = j;
                   break;
             }
       }
 }
+// Try Going Each Cardinal Direction From Start
+let trajectory = null; i=0;
+const compass = Object.keys(connections);
+while ((trajectory===null)&&(i<compass.length)) {
+      dir = compass[i];
+      trajectory = checkPathFromStart(s_i,s_j,dir);
+      i++;
+}
 
-connections = {
-      'N': ['|','L','J'],
-      'E': ['-','L','F'],
-      'S': ['|','7','F'],
-      'W': ['-','J','7']
-};
-
-function checkNorth(i,j,s_i,s_j,trajectory) {
-      if (connections['N'].includes(input[i][j])) {
-            trajectory.push([i-1,j]);
-            if (checkLoop(i-1,j,s_i,s_j)) {
-
+function checkNextNeighbor(trajectory) {
+      const len = trajectory.length;
+      const i = trajectory[len-1][0], j = trajectory[len-1][1];
+      const prev_i = trajectory[len-2][0], prev_j = trajectory[len-2][1]; 
+      const current_pipe = input[i][j];
+      // Check North
+      if (((i-1)>-1)&&((i-1)!=prev_i)) {
+            north_pipe = input[i-1][j];
+            if ((connections['N'].includes(current_pipe)) && (connections['S'].includes(north_pipe))) {
+                  trajectory.push([i-1,j]);
             }
-            checkNorth(i-1,j,s_i,s_j);
-            checkEast(i-1,j,s_i,s_j);
-            checkWest(i-1,j,s_i,s_j);
       }
+      // Check South
+      if (((i+1)<input.length)&&((i+1)!=prev_i)) {
+            south_pipe = input[i+1][j];
+            if ((connections['S'].includes(current_pipe)) && (connections['N'].includes(south_pipe))) {
+                  trajectory.push([i+1,j]);
+            }
+      }
+      // Check West
+      if (((j-1)>-1)&&((j-1)!=prev_j)) {
+            west_pipe = input[i][j-1];
+            if ((connections['W'].includes(current_pipe)) && (connections['E'].includes(west_pipe))) {
+                  trajectory.push([i,j-1]);
+            }
+      }
+      // Check East
+      if (((j+1)<input[0].length)&&((j+1)!=prev_j)) {
+            east_pipe = input[i][j+1];
+            if ((connections['E'].includes(current_pipe)) && (connections['W'].includes(east_pipe))) {
+                  trajectory.push([i,j+1]);
+            }
+      }
+      if (trajectory.length > len) {return trajectory;} else {return null;}
 }
-function checkEast() {
 
+function checkPathFromStart(s_i,s_j,direction) {
+      // Append Start
+      let trajectory = [];
+      trajectory.push([s_i,s_j]);
+      // Append First Pipe 
+      let c_i=s_i, c_j=s_j;
+      if (direction=='N') {c_i -= 1;} else
+      if (direction=='E') {c_j += 1;} else
+      if (direction=='S') {c_i += 1;} else 
+      if (direction=='W') {c_j -= 1;}
+      trajectory.push([c_i,c_j]);
+      // Explore Until Loop or Deadend
+      while (trajectory!=null) {
+            if (checkLoop(trajectory)) {break;}
+            trajectory = checkNextNeighbor(trajectory);
+      }
+      return trajectory;
 }
-function checkSouth() {
 
+function checkLoop(trajectory) {
+      pos = trajectory[trajectory.length-1];
+      pipe = input[pos[0]][pos[1]];
+      return (pipe=='S');
 }
-function checkWest() {
 
-}
-
-
-console.log(s_i);
-console.log(s_j);
-console.log(input[s_i])
+console.log((trajectory.length-1)/2)
---
+++ 10/part-two.js
@@ -0,0 +1,10 @@
+const {input} = require('./input');
+// const input = [
+//       '....|',
+//       '..F-J',
+//       '.FS7.',
+//       '.L-J.',
+//       '.....'
+// ]
+
+//mask = Array(input.length).fill(Array(input[0].length).join('1'));
